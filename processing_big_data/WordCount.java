189efb05a72a170169813fd60b5b2de504cf4e30
Francis Olakangil <fj.olakangil@gmail.com>
2026-02-01 17:05:56 -0500

    sum reducer

--- processing_big_data/WordCount.java
+++ processing_big_data/WordCount.java
@@ -29,4 +29,20 @@ public class WordCount {
       }
     }
   }
+
+  public static class IntSumReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
 }

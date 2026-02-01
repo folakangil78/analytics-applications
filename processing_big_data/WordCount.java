d49763fbf9a81300b665ac90b9a17d2280a416b1
Francis Olakangil <fj.olakangil@gmail.com>
2026-02-01 17:05:25 -0500

    tokenizer mapper

--- processing_big_data/WordCount.java
+++ processing_big_data/WordCount.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.util.StringTokenizer;
 
+import javax.naming.Context;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -12,5 +14,19 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class WordCount {
+  public static class TokenizerMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
 
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString());
+      while (itr.hasMoreTokens()) {
+        word.set(itr.nextToken());
+        context.write(word, one);
+      }
+    }
+  }
 }

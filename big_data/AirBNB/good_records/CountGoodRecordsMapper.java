01f918c72d700139dd3288849d4a9f5109d7a9a8
Francis Olakangil <fj.olakangil@gmail.com>
2026-02-27 18:26:44 -0500

    mapper block

--- big_data/AirBNB/good_records/CountGoodRecordsMapper.java
+++ big_data/AirBNB/good_records/CountGoodRecordsMapper.java
@@ -1,9 +1,33 @@
 import java.io.IOException;
+
+import javax.naming.Context;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class CountGoodRecordsMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text goodKey = new Text("Total number of good lines in AirBNB file:");
 
+    private static final int EXPECTED_FIELDS = 16;
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+
+        String line = value.toString();
+
+        // Skip header
+        if (line.startsWith("id,")) {
+            return;
+        }
+
+        String[] fields = line.split(",");
+
+        if (fields.length == EXPECTED_FIELDS) {
+            context.write(goodKey, one);
+        }
+    }
 }

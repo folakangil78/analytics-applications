0a0210a32c9eb37b22ac3ba6b15f5f52a27aac22
Francis Olakangil <fj.olakangil@gmail.com>
2026-02-27 17:21:37 -0500

    param setup

--- big_data/AirBNB/bad_short_records/CountBadShortRecordsMapper.java
+++ big_data/AirBNB/bad_short_records/CountBadShortRecordsMapper.java
@@ -1,9 +1,21 @@
 import java.io.IOException;
+
+import javax.naming.Context;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class CountBadShortRecordsMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text badKey = new Text("Total number of short lines in AirBNB file:");
 
+    private static final int EXPECTED_FIELDS = 16;
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+
+        String line = value.toString();
 }

c35ee4e48de2b5c547937923a1484be6f06b834d
Francis Olakangil <fj.olakangil@gmail.com>
2026-02-27 16:53:12 -0500

    line counter header

---
+++ big_data/AirBNB/number_of_lines/CountLinesMapper.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class CountLinesMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+}

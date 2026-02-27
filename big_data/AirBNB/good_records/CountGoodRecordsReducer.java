fdfa2cc2dd2dc1d3d469ce1fafffb7f73734ad69
Francis Olakangil <fj.olakangil@gmail.com>
2026-02-27 18:27:21 -0500

    reducer header

---
+++ big_data/AirBNB/good_records/CountGoodRecordsReducer.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class CountGoodRecordsReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+}

59b139ad42ef387d9bbc6b94fd9d59d2f2cc0024
Francis Olakangil <fj.olakangil@gmail.com>
2026-02-27 17:25:35 -0500

    driver block

--- big_data/AirBNB/bad_short_records/CountBadShortRecordsDriver.java
+++ big_data/AirBNB/bad_short_records/CountBadShortRecordsDriver.java
@@ -7,5 +7,23 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class CountBadShortRecordsDriver {
+    public static void main(String[] args) throws Exception {
 
+        Configuration conf = new Configuration();
+
+        Job job = Job.getInstance(conf, "Count Bad Short Records");
+
+        job.setJarByClass(CountBadShortRecordsDriver.class);
+
+        job.setMapperClass(CountBadShortRecordsMapper.class);
+        job.setReducerClass(CountBadShortRecordsReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 }

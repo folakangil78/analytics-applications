c0b8e35ef6a37235679f6027e2c6fa503edb7bd8
Francis Olakangil <fj.olakangil@gmail.com>
2026-02-27 16:53:58 -0500

    mapper block

--- big_data/AirBNB/number_of_lines/CountLinesMapper.java
+++ big_data/AirBNB/number_of_lines/CountLinesMapper.java
@@ -1,9 +1,20 @@
 import java.io.IOException;
+
+import javax.naming.Context;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class CountLinesMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text totalKey = new Text("Total number of lines in AirBNB file:");
 
+    @Override
+    protected void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+
+        context.write(totalKey, one);
+    }
 }

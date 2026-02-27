823d9483ec35cc3d97baad48dc0d25f8996cf011
Francis Olakangil <fj.olakangil@gmail.com>
2026-02-27 00:05:16 -0500

    reducer edits

--- big_data/AirBNB/NeighborhoodReducer.java
+++ big_data/AirBNB/NeighborhoodReducer.java
@@ -4,7 +4,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class NeighborhoodReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-    Override
+    @Override
     protected void reduce(Text key, Iterable<IntWritable> values, Context context)
             throws IOException, InterruptedException {
 

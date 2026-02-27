b54e718be5023644d69cde8a9c2a8fd4116b072a
Francis Olakangil <fj.olakangil@gmail.com>
2026-02-27 17:23:58 -0500

    sum iterator

--- big_data/AirBNB/bad_short_records/CountBadShortRecordsReducer.java
+++ big_data/AirBNB/bad_short_records/CountBadShortRecordsReducer.java
@@ -4,5 +4,16 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class CountBadShortRecordsReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context)
+            throws IOException, InterruptedException {
 
+        int sum = 0;
+
+        for (IntWritable val : values) {
+            sum += val.get();
+        }
+
+        context.write(key, new IntWritable(sum));
+    }
 }

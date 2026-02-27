0d72c7e68253fc32986dab3cba593c607631668f
Francis Olakangil <fj.olakangil@gmail.com>
2026-02-27 17:41:58 -0500

    bad rows longer than field num

---
+++ big_data/AirBNB/bad_long_records/CountBadLongRecordsMapper.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class CountBadLongRecordsMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+}

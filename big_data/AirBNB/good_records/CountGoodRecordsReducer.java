e7e033eb7b577e30269eab6734ece8656439b197
Francis Olakangil <fj.olakangil@gmail.com>
2026-02-27 18:27:41 -0500

    reducer block

--- big_data/AirBNB/good_records/CountGoodRecordsReducer.java
+++ big_data/AirBNB/good_records/CountGoodRecordsReducer.java
@@ -1,8 +1,22 @@
 import java.io.IOException;
+
+import javax.naming.Context;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class CountGoodRecordsReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context)
+            throws IOException, InterruptedException {
 
+        int sum = 0;
+
+        for (IntWritable val : values) {
+            sum += val.get();
+        }
+
+        context.write(key, new IntWritable(sum));
+    }
 }

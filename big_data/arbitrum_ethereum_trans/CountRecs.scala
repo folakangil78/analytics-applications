03b322669a3d51d715b6f50ef76e6bd717fa1135
Francis Olakangil <fj.olakangil@gmail.com>
2026-04-06 22:53:27 -0400

    rdd conversion

--- big_data/arbitrum_ethereum_trans/CountRecs.scala
+++ big_data/arbitrum_ethereum_trans/CountRecs.scala
@@ -1,1 +1,10 @@
 import org.apache.spark.sql.SparkSession
+
+object CountRecs {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder()
+      .appName("Count Records and Explore")
+      .getOrCreate()
+
+    val sc = spark.sparkContext

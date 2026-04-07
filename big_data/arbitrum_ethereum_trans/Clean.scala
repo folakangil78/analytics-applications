e3ba02a9b40c1395bb3fe98b871cb79397022cb6
Francis Olakangil <fj.olakangil@gmail.com>
2026-04-06 22:56:21 -0400

    read in parquet

--- big_data/arbitrum_ethereum_trans/Clean.scala
+++ big_data/arbitrum_ethereum_trans/Clean.scala
@@ -1,2 +1,14 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+
+object Clean {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder()
+      .appName("Clean Arbitrum Data")
+      .getOrCreate()
+
+    val inputPath = "arbitrum_sample/"
+    val outputPath = "hdfs:///user/cleaned_arbitrum/"
+
+    val df = spark.read.parquet(inputPath)

63ffbcd19a16d7b37e567f55419b54dd577bba4d
Francis Olakangil <fj.olakangil@gmail.com>
2026-04-10 23:24:41 -0400

    input to parquets

--- big_data/arbitrum_ethereum_trans/CountRecsv2.scala
+++ big_data/arbitrum_ethereum_trans/CountRecsv2.scala
@@ -1,2 +1,18 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+
+object CountRecs {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder()
+      .appName("Arbitrum Count Records")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    // to parquet directory
+    val inputPath = "/home/fjo2015_nyu_edu/Arbitrum/arbitrum_sample"
+
+    // 1. READ ALL PARQUET FILES
+    val df = spark.read.parquet(inputPath)
+    

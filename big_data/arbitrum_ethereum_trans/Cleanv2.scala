b9d65df15c92f9cffe128386a692d73554ca9a42
Francis Olakangil <fj.olakangil@gmail.com>
2026-04-13 19:16:30 -0400

    hdfs IO

--- big_data/arbitrum_ethereum_trans/Cleanv2.scala
+++ big_data/arbitrum_ethereum_trans/Cleanv2.scala
@@ -1,3 +1,15 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
+
+object Clean {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder()
+      .appName("Clean Arbitrum Data")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    val inputPath = "hdfs:///user/fjo2015_nyu_edu/arbitrum_sample/"
+    val outputPath = "hdfs:///user/fjo2015_nyu_edu/arbitrum_sample_cleaned/"

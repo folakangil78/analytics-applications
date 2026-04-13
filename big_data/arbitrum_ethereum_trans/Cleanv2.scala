9cefe35bca30d5065ac9dcbb3ff3bf692938bdf6
Francis Olakangil <fj.olakangil@gmail.com>
2026-04-13 19:16:53 -0400

    relev cols

--- big_data/arbitrum_ethereum_trans/Cleanv2.scala
+++ big_data/arbitrum_ethereum_trans/Cleanv2.scala
@@ -13,3 +13,25 @@ object Clean {
 
     val inputPath = "hdfs:///user/fjo2015_nyu_edu/arbitrum_sample/"
     val outputPath = "hdfs:///user/fjo2015_nyu_edu/arbitrum_sample_cleaned/"
+
+    // -------------------------------
+    // 1. READ DATA
+    // -------------------------------
+    val df = spark.read.parquet(inputPath)
+
+    // -------------------------------
+    // 2. SELECT ONLY RELEVANT COLUMNS
+    // -------------------------------
+    val selectedDF = df.select(
+      $"MAX_PRIORITY_FEE_PER_GAS_GWEI",
+      $"MAX_FEE_PER_GAS_GWEI",
+      $"GAS_USED",
+      $"VALUE",
+      $"FROM_ADDRESS",
+      $"TO_ADDRESS",
+      $"CONTRACT_ADDRESS",
+      $"DATETIME",
+      $"BLOCK_NUMBER",
+      $"STATUS",
+      $"INPUT"
+    )
